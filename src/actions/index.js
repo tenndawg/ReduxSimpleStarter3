8e96037f6c5aac531259628ea3f01b327507c0d5
tenndawg <nickolas.tenney@gmail.com>
2018-05-14 13:41:59 -0600

    installed axios and setup url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
-const API_KEY = '15145cde6f37382722186c6b85b358b5'
+import axios from 'axios';
+
+const API_KEY = '15145cde6f37382722186c6b85b358b5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
