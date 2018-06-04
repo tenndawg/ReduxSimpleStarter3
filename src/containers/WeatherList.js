9eb26c770609c5af9beef6442fa8ac7f6f8c17b8
tenndawg <nickolas.tenney@gmail.com>
2018-06-04 10:03:24 -0600

    Got the sparklines working

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesBars } from 'react-sparklines';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
@@ -12,7 +12,8 @@ class WeatherList extends Component {
                 <td>{name}</td>
                 <td>
                     <Sparklines height={120} width={180} data={temps}>
-                        <SparklinesLine color="blue" />
+                        <SparklinesLine color={"blue"} />
+                        <SparklinesBars />
                     </Sparklines>
                 </td>
             </tr>
